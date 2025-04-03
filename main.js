0d7d7d68c0b1ff3af96362594685bdde2cf02283
Тьома Бабіч <babichtyoma@gmail.com>
2025-04-03 19:15:54 +0300

    3.0

--- main.js
+++ main.js
@@ -25,28 +25,58 @@ let footer = document.querySelector('footer')
 
 let klass = document.querySelectorAll('.klass')
 
-function mouserr(){
-body.style.backgroundColor = '#000000';
-header.style.backgroundColor = '#ffffff'
-title.style.color = '#ffffff'
-ab.style.color = '#ffffff'
-ab1.style.color = '#ffffff'
-ab2.style.color = '#ffffff'
-them.style.backgroundColor = '#000000'
-them.style.color = '#ffffff'
-header2.style.backgroundColor = '#000000'
-header2.style.color = '#ffffff'
-header1.style.backgroundColor = '#000000'
-header1.style.color = '#ffffff'
-footer.style.backgroundColor = '#ffffff'
-for (let i=0;i<h3.length;i+=1){
-   h3[i].style.background = "#ffffff"
-   h3[i].style.color  = "#000000"
-   console.log("привіт")
-}
-for (let i=0;i<klass.length;i+=1){
-    klass[i].style.color  = "#000000"
-    console.log("привіт")
- }
-}
 
+let them2 = "white"
+
+   function  mouserr(){
+      if (them2 == "white"){
+      body.style.backgroundColor = '#000000';
+      header.style.backgroundColor = '#ffffff'
+      title.style.color = '#ffffff'
+      ab.style.color = '#ffffff'
+      ab1.style.color = '#ffffff'
+      ab2.style.color = '#ffffff'
+      them.style.backgroundColor = '#000000'
+      them.style.color = '#ffffff'
+      header2.style.backgroundColor = '#000000'
+      header2.style.color = '#ffffff'
+      header1.style.backgroundColor = '#000000'
+      header1.style.color = '#ffffff'
+      footer.style.backgroundColor = '#ffffff'
+      for (let i=0;i<h3.length;i+=1){
+         h3[i].style.background = "#ffffff"
+         h3[i].style.color  = "#000000"
+         console.log("привіт")
+      }
+      for (let i=0;i<klass.length;i+=1){
+          klass[i].style.color  = "#000000"
+          console.log("привіт")
+      }
+      them2 = "black"
+}
+      else {
+         body.style.backgroundColor = '#ffffff';
+         header.style.backgroundColor = '#000000'
+         title.style.color = '#000000'
+         ab.style.color = '#000000'
+         ab1.style.color = '#000000'
+         ab2.style.color = '#000000'
+         them.style.backgroundColor = '#ffffff'
+         them.style.color = '#000000'
+         header2.style.backgroundColor = '#ffffff'
+         header2.style.color = '#000000'
+         header1.style.backgroundColor = '#ffffff'
+         header1.style.color = '#000000'
+         footer.style.backgroundColor = '#000000'
+         for (let i=0;i<h3.length;i+=1){
+            h3[i].style.background = "#000000"
+            h3[i].style.color  = "#ffffff"
+            console.log("привіт")
+         }
+         for (let i=0;i<klass.length;i+=1){
+             klass[i].style.color  = "#ffffff"
+             console.log("привіт")
+         }
+      them2 = "white"
+      }
+}
